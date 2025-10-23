854e27c17f99a9d87c1cf6b9cfc3be7c3bbd37e9
de oliveira emma <emma.deoliveira@ynov.com>
2025-10-23 10:51:57 +0200

    Premier commit

---
+++ Documents/Ynov/Moka/Moka/MokaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MokaApp.swift
+//  Moka
+//
+//  Created by Emma De Oliveira on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MokaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
